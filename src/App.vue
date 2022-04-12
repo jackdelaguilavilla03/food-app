07ec47efa39cace40e03c7331193b27f5bfbd3ad
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-04-12 16:17:13 -0500

    add home and nav-bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+		<div id="app">
+				<div class="navbar-container">
+						<div class="navbar-content">
+								<router-link to="/">
+<box-icon name='home-alt-2' color="gray"></box-icon>
+								</router-link>
+								<router-link to="/shop">
+<box-icon name='basket' color="gray"></box-icon>
+								</router-link> 
+								<router-link to="/bookmark">
+<box-icon name='bookmark' color="gray"></box-icon>
+								</router-link>  
+								<router-link to="/profile">
+<box-icon name='user-circle' color="gray"></box-icon>
+								</router-link> 
+						</div>
+				</div>
+				<router-view/>
+		</div>
 </template>
 
+<script>
+// @ is an alias to /src
+//import @HomeView  from 
+
+</script>
 <style lang="less">
+
+.navbar-container {
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+
+		.navbar-content {
+				padding: 20px;
+				display: flex;
+				justify-content: space-between;
+
+				a {
+						color: rgba(163,163,163);
+						&.router-link-active {
+								color: rgba(255, 131, 62);
+						}
+				}
+		}
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+		padding: 0;
+		margin: 0;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="home">
+				<div class="header">
+						<h2 class="title">Hi <b>Jack!</b> </h2>
+						<div class="input">
+								<input type="text" class="" placeholder="Find what you want">
+								<div class="icon-input">
+										<box-icon name='search' color="lightgrey"></box-icon>
+								</div>
+						</div>
+						<div class="combo-header">
+								<div class="text">
+								<h2>Combo Cheese</h2>
+								<p> Cheese Burger x2 <br/> New Users</p>
+								</div>
+								<div class="img">
+										<img src="https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4115.png">
+								</div>
+						</div>
+						<div class="scroll-main"> 
+								<div class="scroll-content">
+										<div class="avatar">
+										</div>
+								</div>
+						</div>
+				</div>
   </div>
 </template>
 
@@ -11,4 +35,67 @@ export default {
   components: {
   }
 }
-</script>
+</script> 
+
+<style lang="less">
+
+.home {
+		margin: 20px;
+}
+.header {
+		.scroll-main {}
+		.combo-header {
+				margin-top: 20px;
+				background-color: orange;
+				color: white;
+				padding: 20px;
+				border-radius: 20px;
+				display: flex;
+				.img {
+						width: 50%;
+						object-fit: cover;
+						img {
+								width:100%;
+						}
+				}
+				.text {
+						margin: auto;
+						width: 50%;
+						h2 {
+								font-size: 7.7vw;
+								font-family:'Rubik Moonrocks', cursive;;
+								font-weight: 400;
+						}
+						p {
+								font-size: 4vw;
+						}
+				}
+		}
+		.input {
+				margin-top: 15px;
+				display: flex;
+				position: relative;
+				align-items: center;
+				input {
+						width: 100%;
+						background-color: rgba(250,248,248);
+						border: 0px;
+						outline: none;
+						padding: 10px;
+						padding-left: 30px;
+						border-radius: 10px;
+				}
+				.icon-input {
+						position: absolute;
+						margin-right: 8px;
+						margin-top: 6px;
+						margin-left: 4px;
+				}
+		}
+
+		.title {
+				font-weight: 350;
+		}
+}
+</style>
+
