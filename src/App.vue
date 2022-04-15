1e989c987368a64221d0d82dfbeeecc773068909
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-04-14 22:59:56 -0500

    xd

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,57 @@
 <template>
-		<div id="app">
-				<div class="navbar-container">
-						<div class="navbar-content">
-								<router-link to="/">
-<box-icon name='home-alt-2' color="gray"></box-icon>
-								</router-link>
-								<router-link to="/shop">
-<box-icon name='basket' color="gray"></box-icon>
-								</router-link> 
-								<router-link to="/bookmark">
-<box-icon name='bookmark' color="gray"></box-icon>
-								</router-link>  
-								<router-link to="/profile">
-<box-icon name='user-circle' color="gray"></box-icon>
-								</router-link> 
-						</div>
-				</div>
-				<router-view/>
-		</div>
+  <div id="app">
+    <div class="navbar-container">
+      <div class="navbar-content">
+        <router-link to="/">
+          <box-icon name="home-alt-2" color="gray"></box-icon>
+        </router-link>
+        <router-link to="/shop">
+          <box-icon name="basket" color="gray"></box-icon>
+        </router-link>
+        <router-link to="/bookmark">
+          <box-icon name="bookmark" color="gray"></box-icon>
+        </router-link>
+        <router-link to="/profile">
+          <box-icon name="user-circle" color="gray"></box-icon>
+        </router-link>
+      </div>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
 </script>
 <style lang="less">
-
 .navbar-container {
-		position: fixed;
-		bottom: 0;
-		width: 100%;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 
-		.navbar-content {
-				padding: 20px;
-				display: flex;
-				justify-content: space-between;
+  .navbar-content {
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
 
-				a {
-						color: rgba(163,163,163);
-						&.router-link-active {
-								color: rgba(255, 131, 62);
-						}
-				}
-		}
+    a {
+      color: rgba(163, 163, 163);
+      &.router-link-active {
+        color: rgba(255, 131, 62);
+      }
+    }
+  }
 }
 
 #app {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
 
 * {
-		padding: 0;
-		margin: 0;
+  padding: 0;
+  margin: 0;
 }
 </style>
